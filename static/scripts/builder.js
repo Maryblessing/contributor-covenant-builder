d0b189976caf8b39e0b3533049861e4e3b3cb13f
CoralineAda <coraline@idolhands.com>
2025-06-27 21:44:10 -0500

    Correctly fetch the code of conduct file

--- static/scripts/builder.js
+++ static/scripts/builder.js
@@ -8,7 +8,7 @@ const readTemplate = async (url) => {
 
 const initializeTemplate = async (version) => {
   const sourceUrl =  window.location.href.replace("adopt/",`version/${version}/code_of_conduct/code_of_conduct.md`)
-  const content = await readTemplate(sourceUrl)
+  const content = readTemplate(sourceUrl)
   const template = document.createElement('template')
   template.id = 'template'
   template.innerHTML = content
@@ -18,15 +18,10 @@ const initializeTemplate = async (version) => {
 
 const populateReporting = (placeholder, content) => {
   const template = document.getElementsByName('template')[0]
-  const contents = template.innerHtml
-  var updatedContent = contents.replace(placeholder, content)
-  populateTemplate(updatedContent)
-}
-
-const populateRemedies = (content) => {
-  const template = document.getElementById('template')
-  const contents = template.innerHtml
-  populateTemplate(contents.replace("[ Update with your custom enforcement policy ]", content))
+  const contents = template.innerHTML
+  const updatedContent = contents.replace(placeholder, content)
+  template.innerHTML = updatedContent
+  return template
 }
 
 /**
