0b63a834223fc9e02594fb46426f99dcdc1aaa37
CoralineAda <coraline@idolhands.com>
2025-06-27 20:18:15 -0500

    Began on JS for builder

--- static/scripts/builder.js
+++ static/scripts/builder.js
@@ -1,3 +1,17 @@
+async function readTemplate(versionUrl) {
+  try {
+    const response = await fetch(versionUrl);
+    if (!response.ok) {
+      throw new Error('HTTP error when retrieving ${versionUrl}: ${response.status}');
+    }
+    const text = await response.text();
+    return text;
+  } catch (error) {
+    console.error("Error fetching the URL #{versionUrl}:", error);
+    return null;
+  }
+}
+
 /**
  * Copies text to clipboard.
  * Uses async browser Clipboard API.
@@ -7,28 +21,36 @@
  * @param {string} text
  */
 const copyToClipboard = (text = ``) => {
-  navigator.clipboard.writeText(text).then(
-    function () {
-      console.log('Async: Copied to clipboard!')
-    },
-    function (err) {
-      console.error('Async: Could not copy text: ', err)
-    }
-  )
+  navigator.clipboard.writeText(text)
 }
 
-// cr is just a shortform for document.createElement
-const cr = (...args) => document.createElement(...args)
+const initializeTemplate = (version) => {
+  const sourceUrl =  window.location.href.replace()
+  const content = readTemplate(sourceUrl)
+}
 
 /**
  * buildHTML instantiates HTML nodes from a string.
- * @param {string} htmlString Any valid html.
- * @returns {Object[]} proper HTML nodes.
+ * @param {string} content Any text.
+ * @returns {Object[]} An HTML node containing that content.
  */
-const buildHTML = (htmlString) => {
-  const template = cr('template')
-  template.innerHTML = htmlString
+const populateTemplate = (content) => {
+  const template = document.createElement('template')
+  template.innerHTML = content
   return template.content.cloneNode(true)
+}
+
+const populateReporting = (placeholder, content) => {
+  const template = document.getElementsByName('template')[0]
+  const contents = template.innerHtml
+  var updatedContent = contents.replace(placeholder, content)
+  populateTemplate(updatedContent)
+}
+
+const populateRemedies = (content) => {
+  const template = document.getElementsByName('template')[0]
+  const contents = template.innerHtml
+  populateTemplate(contents.replace("[ Update with your custom enforcement policy ]", content))
 }
 
 /**
